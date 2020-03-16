f4949fe503c11f9f4e9f894999ffec370a16a85b
Wanjin Yoo <dksladjsla@gmail.com>
2020-03-16 05:57:13 +0000

    function exercises

---
+++ howmanyhundreds.js
@@ -0,0 +1,10 @@
+
+var howManyHundreds = function(num){
+	let hund = num / 100;
+	return Math.floor(hund); 
+}
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
